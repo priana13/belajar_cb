58357b3ed8e5b4bd7a6a39583b8835d900d1fae2
priana <herian.sap@gmail.com>
2021-02-02 22:27:36 +0700

    make auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Platform Donasi
                 </div>
 
                 <div class="links">
