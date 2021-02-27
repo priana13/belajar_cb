532f564d7e63efa7f0d61d3f604f5467deb73bec
priana <herian.sap@gmail.com>
2021-02-27 17:23:22 +0700

    Tampilan halaman terimakasih Oke

--- resources/views/front/terimakasih.blade.php
+++ resources/views/front/terimakasih.blade.php
@@ -8,49 +8,80 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Formulir Donasi</title>
-  </head>
-  <body>
+    <title>Terimakasih</title>
+  </head >
+  <body class="bg-light">
+ <div class="bg-light">
+
+     <div class="row mb-3"> <!-- row navbar -->
+            <div class="col-md">
+                <!-- Just an image -->
+                <nav class="navbar navbar-light bg-white">
+                <a class="navbar-brand" href="#">
+                    <!-- Logo <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""> -->
+                <h2>Logo</h2>
+                </a>
+                </nav>
+            </div>
+        </div> <!-- akhir row navbar -->
 
 
-<div class="container">
-<center>
+    <div class="container">
+        <div class="bg-white text-center p-3">
+        <hr class="border border-warning">
+        
+            <h4 class="h4">Yuk Selangkah Lagi Donasimu Berhasil </h4>
+            <p class="text mb-5">No. Invoice : INV-210227733944</p>
+            
 
-    <div class="row mb-3"> <!-- row navbar -->
-        <div class="col-md">
-            <!-- Just an image -->
-            <nav class="navbar navbar-light bg-light">
-            <a class="navbar-brand" href="#">
-                <!-- Logo <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt=""> -->
-            <h2>Logo</h2>
-            </a>
-            </nav>
-        </div>
-    </div> <!-- akhir row navbar -->
+            <p class="text mb-0 pb-0">Silahkan transfer ke rekening a/n</p>
+            <h5 class="h5 mt-0 pt-0">Yayasan A - KCP Bandung</h5>
 
-    <div class="row"> <!-- awal row formulir -->
-        <div class="col-md">
-            <h4 class="mb-3">Terimakasih <strong>Priana</strong></h4>
-
-            <h5>Berikut Detail Donasi Anda:</h5>
-            <h2 class="h2">50.000</h2>
+            <div class="flex border border-gray rounded bg-gray-600 items-center p-3 mt-3 mb-3">
+              <p>05588888</p>
+             </div>
 
 
-        <div class="alert alert-primary">        
-            Mohon transfer sampai 3 Angka terakhir agar bisa dicek secara otomatis.
-        </div>
-            <h5>Piliha Metode Transfer:</h5>
-            <h3>Bank Mandiri : <strong>058889994</strong></h3>
-            <p>a.n. Yay. Kita Pasti Bisa</p>
-            <button type="submit" class="btn btn-danger btn-lg mb-4">Donasi Lagi</button>
-        </div>  <!-- akhir md-8 -->
-    </div> <!-- akhir row formulir -->
-
-</center>
+            <p>Transfer sesuai nominal berikut</p>
+            <h3 class ="text-base">Rp. 10.057</h3>
 
 
-</div><!-- akhir div container -->
+            <div class="alert alert-warning" role="alert">
+                <span class="oi oi-info"></span>
+                <p class="text"><strong>PENTING!</strong> Mohon Transfer tepat <strong>3 Digit Terakhir</strong> yang tertera agar bisa di cek oleh system ya Kak.</p>
+            </div>
 
+
+            <div >
+                <table class="table table-price rounded border border-gray bg-gray p-3">
+                  <tr>
+                    <td>Donasi</td>
+                    <td>50.000</td>
+                  </tr>
+                  <tr>
+                    <td>Kode Unik</td>
+                    <td>057</td>
+                  </tr>
+                </table>
+            </div>
+
+            <p class="text text-small"><i>*Kode Unik akan didonasikan</i></p>
+
+
+            <hr class="border border-warning">
+
+
+
+        <button class="btn btn-primary btn-block btn-lg">Share ke Facebook</button>
+        <button class="btn btn-success btn-block btn-lg">Share ke Whatsapp</button>
+        
+
+          </div>
+
+
+    </div>
+ 
+ </div> 
 
 
 
